e024b96f32396853be53c37bf98b1b63196440b8
Krzysztof Gryc <hello@krzysztofg.pl>
2019-10-22 14:34:28 +0200

    add Display jsx structure and styles

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import '../css/Display.css'
 
-const Display = () => {
+const Display = (props) => {
     return (
         <>
-            <div>Display</div>
+            <div className="display">
+                <table>
+                    <tbody>
+                        <tr className="display__header">
+                            <th className="display__header-item">City</th>
+                            <th className="display__header-item">Time</th>
+                            <th className="display__header-item">Humidity</th>
+                            <th className="display__header-item">Temp</th>
+                        </tr>
+                        {props.results.map(item => <tr key={item.key} className="display__data">
+                            <td className="display__data-item" >{item.city}</td>
+                            <td className="display__data-item" >{item.time}</td>
+                            <td className="display__data-item" >{item.humidity}</td>
+                            <td className="display__data-item" >{item.temp}</td>
+                        </tr>)}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
